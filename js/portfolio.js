bf2d2b2489e1befe88c78ae3c352f487ed3b3334
Franco Juri <francojuri22@gmail.com>
2024-01-24 01:01:58 -0300

    lazer web project added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,9 @@
 const freeGamesImg = document.querySelector('.free-games-img');
 const meeurhaApp = document.querySelector('.meeurha-com-img');
 const palmerusEs = document.querySelector('.palmerus-es-img');
-const journalAppImg = document.querySelector('.journal-app-img');
 const calendarAppImg = document.querySelector('.calendar-app-img');
+const journalAppImg = document.querySelector('.journal-app-img');
+const lazerwebCursoImg = document.querySelector('.lazerweb-curso-img');
 const weatherAppImg = document.querySelector('.weather-app-img');
 const gifExpertAppImg = document.querySelector('.gif-expert-app-img');
 const todoAppImg = document.querySelector('.todo-app-img');
@@ -16,6 +17,7 @@ function changeImgs(){
         palmerusEs.src = '../assets/images/palmerus-es-responsive.webp';
         journalAppImg.src = '../assets/images/journal-app-responsive.webp';
         calendarAppImg.src = '../assets/images/calendar-app-responsive.webp';
+        lazerwebCursoImg.src = '../assets/images/lazerweb-curso-responsive.webp';
         weatherAppImg.src = '../assets/images/weather-app-responsive.webp';
         gifExpertAppImg.src = '../assets/images/gif-expert-app-responsive.webp';
         todoAppImg.src = '../assets/images/todo-app-responsive.webp';
@@ -25,6 +27,7 @@ function changeImgs(){
         palmerusEs.src = '../assets/images/palmerus-es.webp';
         journalAppImg.src = '../assets/images/journal-app.webp';
         calendarAppImg.src = '../assets/images/calendar-app.webp';
+        lazerwebCursoImg.src = '../assets/images/lazerweb-curso.webp';
         weatherAppImg.src = '../assets/images/weather-app.webp';
         gifExpertAppImg.src = '../assets/images/gif-expert-app.webp';
         todoAppImg.src = '../assets/images/todo-app.webp';
